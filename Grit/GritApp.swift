a0c157a754a795343d5781ffbcb559e0fe606f7f
Dalfonso06 <danielalfons06@gmail.com>
2024-01-16 19:13:38 -0500

    Initial Commit

---
+++ Grit/GritApp.swift
@@ -0,0 +1,17 @@
+//
+//  GritApp.swift
+//  Grit
+//
+//  Created by Daniel Alfonso on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
