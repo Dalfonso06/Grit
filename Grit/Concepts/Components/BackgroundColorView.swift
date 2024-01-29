5304678c2a6e1b21bb104e21a7d1bd466950bba9
Dalfonso06 <danielalfonso06@gmail.com>
2024-01-28 20:37:19 -0500

    Updated background colors.

--- Grit/Concepts/Components/BackgroundColorView.swift
+++ Grit/Concepts/Components/BackgroundColorView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct BackgroundColorView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea(.all)
+            
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color("BackgroundColor"))
+                .frame(width: 350, height: 250)
+                .shadow(color: Color("LightShadow"), radius: 8, x: -8, y: -8)
+                .shadow(color: Color("DarkShadow"), radius: 8, x: 8, y: 8)
+        }
     }
 }
 
