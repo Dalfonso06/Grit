9596283fae0cd30bca691fd0da34380288ce4cbc
Dalfonso06 <danielalfonso06@gmail.com>
2024-01-28 20:36:47 -0500

    Updated background color and created background view to present the new contrasting shadows.

--- Grit/Concepts/Components/BackgroundColorView.swift
+++ Grit/Concepts/Components/BackgroundColorView.swift
@@ -2,32 +2,14 @@
 //  BackgroundColorView.swift
 //  Grit
 //
-//  Created by Daniel Alfonso on 1/27/24.
+//  Created by Daniel Alfonso on 1/28/24.
 //
 
 import SwiftUI
 
 struct BackgroundColorView: View {
     var body: some View {
-        ZStack {
-            Color("BackgroundColor")
-                .ignoresSafeArea()
-            
-            RoundedRectangle(cornerRadius: 25)
-                .frame(width: 325, height: 215)
-                .shadow(
-                    color: Color("BackgroundColor"),
-                    radius: 8,
-                    x: -8,
-                    y: -8)
-                .shadow(
-                    color: Color("AccentColor"),
-                    radius: 8,
-                    x: 8,
-                    y: 8
-                )
-                .foregroundColor(Color("BackgroundColor"))
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
