f909f67bc24706d95cfd9bccabdd01c3d2cbb73d
Dalfonso06 <danielalfonso06@gmail.com>
2024-04-23 19:48:24 -0400

    Created constants for styling.

---
+++ Grit/Constants/DesignConstants.swift
@@ -0,0 +1,13 @@
+//
+//  DesignConstants.swift
+//  Grit
+//
+//  Created by Daniel Alfonso on 4/20/24.
+//
+
+import Foundation
+
+struct DesignConstants {
+    static let padding: CGFloat = 15
+    static let cornerRadius: CGFloat = 15
+}
