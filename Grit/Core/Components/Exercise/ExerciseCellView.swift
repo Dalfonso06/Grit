70a506771fcc68ff848eb24ba7ff373699863a12
Dalfonso06 <danielalfonso06@gmail.com>
2024-07-13 18:21:04 -0400

    Added mulitple Exercise cards to view.

---
+++ Grit/Core/Components/Exercise/ExerciseCellView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseCellView.swift
+//  Grit
+//
+//  Created by Daniel Alfonso on 5/21/24.
+//
+
+import SwiftUI
+
+struct ExerciseCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseCellView()
+}
