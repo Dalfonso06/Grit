7d445eb41405eed721a077751e95b4bbf1cadf61
Dalfonso06 <danielalfonso06@gmail.com>
2024-10-12 13:09:51 -0400

    Initialized the profile information view.

---
+++ Grit/Core/Profile/ProfileInfo/ProfileInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileInfoView.swift
+//  Grit
+//
+//  Created by Daniel Alfonso on 10/12/24.
+//
+
+import SwiftUI
+
+struct ProfileInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileInfoView()
+}
---
+++ Grit/Core/Profile/ProfileInfo/ProfileInfoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileInfoViewModel.swift
+//  Grit
+//
+//  Created by Daniel Alfonso on 10/12/24.
+//
+
+import Foundation
